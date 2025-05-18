ec3444424aa58b5583a370f7660257b806ad9b38
RodrigoPasten <rodrigo.pasten.c@gmail.com>
2025-05-17 20:55:10 -0400

    add featured section

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from blog_app.models import Category, Blog
+
+
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    featured_post = Blog.objects.filter(is_featured = True)
+    post = Blog.objects.filter(is_featured = False, status = 'Publicado')
+    context = {
+        'categories':categories,
+        'featured_post':featured_post,
+        'post':post
+    }
+    return render(request, 'home.html', context=context)
